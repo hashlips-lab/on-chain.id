f29a1fc792cbf34eab267725cc03e8b96b7689c5
Supun Lakmal <supunlakmal61@gmail.com>
2022-10-07 22:47:08 +0530

    💄 Dashboard desktop UI fix

--- nextjs/components/PermissionRequestLinkAndCode/PermissionRequestLinkAndCode.tsx
+++ nextjs/components/PermissionRequestLinkAndCode/PermissionRequestLinkAndCode.tsx
@@ -1,6 +1,29 @@
+import Button from "../Button/Button";
 import styles from "./PermissionRequestLinkAndCode.module.scss";
+import Image from "next/image";
 const PermissionRequestLinkAndCode = () => {
-  return <div>PermissionRequestLinkAndCode</div>;
+  return (
+    <div className={styles.permissionRequestLinkAndCode}>
+      <div className={styles.editor}>Code</div>
+      <div className={styles.buildBtn}>
+        <Button type="borderBlueBgBlueTextWhite" onClick={() => {}} size={"lg"}>
+          Build Link
+        </Button>
+      </div>
+      <div className={styles.link}>
+        <span>
+          https://www.Final/URL/OF/Permissionrequest.com/w.Final/URL/OF/Permissionrequest.com/
+        </span>
+
+        <Image
+          src="./images/icon/copy.svg"
+          width={36}
+          height={36}
+          alt="On chain ID"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default PermissionRequestLinkAndCode;
