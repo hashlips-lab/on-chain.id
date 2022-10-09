01129906098d3d23fb154429c963b311de7ba250
Supun Lakmal Abesekara <supunlakmal61@gmail.com>
2022-10-09 20:21:22 +0530

    User dashboard providers (#13)
    * 💄 Update Provider UI
    
    * 💄 Update UI
    
    * 💄 px to rem

---
+++ nextjs/pages/userDashboardProviders.tsx
@@ -0,0 +1,101 @@
+import type { NextPage } from "next";
+import Button from "../components/Button/Button";
+import Nav from "../components/nav/Nav";
+import RightSideContentBox from "../components/RightSideContentBox/RightSideContentBox";
+import TopNavBar from "../components/TopNavBar/TopNavBar";
+import styles from "../styles/UserDashboardProviders.module.scss";
+import Image from "next/image";
+
+const KEY_LIST = [
+  "0xde3B22caAaD25e65C839c2A3d852d665669EdD5c",
+  "0xde3B22caAaD25e65C839c2A3d852d665669EdD5c",
+  "0xde3B22caAaD25e65C839c2A3d852d665669EdD5c",
+];
+
+const UserDashboardProviders: NextPage = () => {
+  return (
+    <div className={styles.userDashboardLinks}>
+      <Nav />
+      <RightSideContentBox>
+        <TopNavBar
+          firstBtnClass="borderBlueBgWhiteTextBlue"
+          firstBtnContent={"TEST"}
+          firstBtnOnClick={() => {}}
+          mainTitle="Provider Dashboard"
+          secondBtnClass="borderBlueBgBlueTextWhite"
+          secondBtnContent={"CREATE"}
+          secondBtnOnClick={() => {}}
+          subTitle="0xde3B22caAaD25e65C839c2A3d852d665669EdD5c"
+        />
+        <div className={styles.midContent}>
+          <div className={styles.subBtnTitleWrapper}>
+            <div className={styles.title}>New Permission Request Link</div>
+            <div className={styles.button}>
+              <Button
+                type="borderRedBgRedTextWhite"
+                onClick={() => {}}
+                size={"md"}
+              >
+                <div className={styles.btnContent}>
+                  <span>REMOVE ALL</span>
+                  <div>
+                    <Image
+                      src={"./images/icon/close.svg"}
+                      width={16}
+                      height={16}
+                      alt={"Remove"}
+                    />
+                  </div>
+                </div>
+              </Button>
+            </div>
+          </div>
+
+          <ul>
+            {KEY_LIST.map((key, index) => {
+              return (
+                <li key={index}>
+                  <div className={styles.listItem}>
+                    <span className={styles.keyText}>{key}</span>
+                    <div className={styles.btnWrapper}>
+                      <div className={styles.firstBtn}>
+                        <Button
+                          type="borderRedBgWhiteTextRed"
+                          onClick={() => {}}
+                          size={"sm"}
+                        >
+                          <div className={styles.btnContent}>
+                            <span>REMOVE</span>
+                            <div>
+                              <Image
+                                src={"./images/icon/closeRed.svg"}
+                                width={16}
+                                height={16}
+                                alt={"Remove"}
+                              />
+                            </div>
+                          </div>
+                        </Button>
+                      </div>
+                      <div className={styles.secondBtn}>
+                        <Button
+                          type="borderBlueBgWhiteTextBlue"
+                          onClick={() => {}}
+                          size={"sm"}
+                        >
+                          VIEW
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </RightSideContentBox>
+    </div>
+  );
+};
+
+export default UserDashboardProviders;
